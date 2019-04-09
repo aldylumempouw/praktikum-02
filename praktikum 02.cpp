af6e25e4e48e3b96c7e54dbd4bd835341e7ad66a
aldylumempouw <48673578+aldylumempouw@users.noreply.github.com>
2019-04-09 21:43:08 +0800

    Add files via upload

---
+++ praktikum 02.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+#include <conio.h>
+
+main()
+{
+char k1,k2;
+k1='A';
+cout <<"nilai variabel K1 adalah %c \n" <<k1;
+cout <<"nilai variabel K2 dalam bentuk angka = %d \n"<<k2;
+getch();
+return 0;
+}
